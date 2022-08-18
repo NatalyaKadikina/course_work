afc0b7935aa106042c9ac8fbc19e54f6aec209f1
Наталья Кадикина <natalya92filipp@gmail.com>
2022-08-18 20:15:33 +0300

    Базовая сложность

---
+++ src/ru/skypro/Employee.java
@@ -0,0 +1,51 @@
+package ru.skypro;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String patronymic;
+    private int id;
+    private int departmentNumber;
+    private int salary;
+
+    public Employee (int id, String lastName, String firstName, String patronymic, int departmentNumber, int salary) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.patronymic = patronymic;
+        this.departmentNumber = departmentNumber;
+        this.salary = salary;
+        this.id = id;
+    }
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getPatronymic() {
+        return patronymic;
+    }
+    public int getDepartmentNumber() {
+        return departmentNumber;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    public void setDepartmentNumber(int departmentNumber) {
+        this.departmentNumber = departmentNumber;
+    }
+
+    @Override
+    public String toString() {
+        return id + ":" + lastName + ":" + lastName + ":" + departmentNumber + ":" + salary;
+    }
+}
--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,66 @@
 package ru.skypro;
 
 public class Main {
+    static int count = 1;
 
     public static void main(String[] args) {
-	// write your code here
+        Employee[] employees = new Employee[]
+        {
+            new Employee(count++,"Ivanov", "Ivan", "Ivanovich", 1, 45000),
+            new Employee(count++,"Ivanov2", "Ivan2", "Ivanovich2", 2, 55000),
+            new Employee(count++,"Ivanov3", "Ivan3", "Ivanovich3", 3, 65000),
+            new Employee(count++,"Ivanov4", "Ivan4", "Ivanovich4", 4, 75000),
+            new Employee(count++,"Ivanov5", "Ivan5", "Ivanovich5", 5, 85000),
+            new Employee(count++,"Ivanov6", "Ivan6", "Ivanovich6", 1, 40000),
+            new Employee(count++,"Ivanov7", "Ivan7", "Ivanovich7", 2, 50000),
+            new Employee(count++,"Ivanov8", "Ivan8", "Ivanovich8", 3, 60000),
+            new Employee(count++,"Ivanov9", "Ivan9", "Ivanovich9", 4, 70000),
+            new Employee(count++,"Ivanov10", "Ivan10", "Ivanovich10", 5, 80000)
+        };
+
+        GetEmployees(employees);
+        System.out.println(GetAllSalaries(employees));
+        GetMinSalary(employees);
+        GetMaxSalary(employees);
+        GetAvgSalary(employees);
+        GetFIO(employees);
+    }
+
+    static void GetEmployees(Employee[] employees){
+        for(Employee emp : employees)
+            System.out.println(emp);
+    }
+
+    static int GetAllSalaries(Employee[] employees){
+        int all = 0;
+        for(Employee emp : employees)
+            all += emp.getSalary();
+        return all;
+    }
+
+    static void GetMinSalary(Employee[] employees){
+        int min = employees[employees.length - 1].getSalary();
+        for(Employee emp : employees)
+            if(min > emp.getSalary())
+                min = emp.getSalary();
+        System.out.println(min);
+    }
+
+    static void GetMaxSalary(Employee[] employees){
+        int max = employees[employees.length - 1].getSalary();
+        for(Employee emp : employees)
+            if(max < emp.getSalary())
+                max = emp.getSalary();
+        System.out.println(max);
+    }
+
+    static void GetAvgSalary(Employee[] employees){
+        int avg = GetAllSalaries(employees) / employees.length;
+        System.out.println(avg);
+    }
+
+    static void GetFIO(Employee[] employees){
+        for(Employee emp : employees)
+            System.out.println(emp.getFirstName() + " " + emp.getLastName() + " " + emp.getPatronymic());
     }
 }
