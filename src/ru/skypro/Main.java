a635944d9da90ea65347455940af03ae7d7dbcbe
Наталья Кадикина <natalya92filipp@gmail.com>
2022-08-18 23:01:43 +0300

    Повышенная сложность

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,8 @@
 package ru.skypro;
 
 public class Main {
-    static int count = 1;
-
+    private static int count = 1;
+    private static int indexing = 25;
     public static void main(String[] args) {
         Employee[] employees = new Employee[]
         {
@@ -24,6 +24,18 @@ public class Main {
         GetMaxSalary(employees);
         GetAvgSalary(employees);
         GetFIO(employees);
+
+        IndexingSalary(employees, indexing);
+        GetMinSalaryDepartment(employees, 2);
+        GetMaxSalaryDepartment(employees, 2);
+        System.out.println(GetAllSalariesDepartment(employees, 2));
+        GetAvgSalaryDepartment(employees, 2);
+        IndexingSalaryDepartment(employees, 2, indexing);
+        GetFIODepartment(employees, 2);
+        System.out.println();
+        GetMinSalaryEmployee(employees, 50000);
+        System.out.println();
+        GetMaxSalaryEmployee(employees, 60000);
     }
 
     static void GetEmployees(Employee[] employees){
@@ -62,5 +74,93 @@ public class Main {
     static void GetFIO(Employee[] employees){
         for(Employee emp : employees)
             System.out.println(emp.getFirstName() + " " + emp.getLastName() + " " + emp.getPatronymic());
+    }
+
+    static  void IndexingSalary(Employee[] employees, int indexing) {
+        for(Employee emp : employees) {
+            int newSalary = emp.getSalary() + Math.round(emp.getSalary() * indexing / 100);
+            emp.setSalary(newSalary);
+        }
+    }
+
+    static void GetMinSalaryDepartment(Employee[] employees, int department) {
+        int min = 0;
+        for(Employee emp : employees) {
+            if(emp.getDepartmentNumber() == department) {
+                min = emp.getSalary();
+                break;
+            }
+        }
+        for(Employee emp : employees) {
+            if(emp.getDepartmentNumber() == department) {
+                if (min > emp.getSalary())
+                    min = emp.getSalary();
+            }
+        }
+        System.out.println(min);
+    }
+
+    static void GetMaxSalaryDepartment(Employee[] employees, int department) {
+        int max = 0;
+        for(Employee emp : employees) {
+            if(emp.getDepartmentNumber() == department) {
+                max = emp.getSalary();
+                break;
+            }
+        }
+        for(Employee emp : employees) {
+            if(emp.getDepartmentNumber() == department) {
+                if (max < emp.getSalary())
+                    max = emp.getSalary();
+            }
+        }
+        System.out.println(max);
+    }
+
+    static int GetAllSalariesDepartment(Employee[] employees, int department){
+        int all = 0;
+        for(Employee emp : employees) {
+            if(emp.getDepartmentNumber() == department)
+                all += emp.getSalary();
+        }
+        return all;
+    }
+
+    static void GetAvgSalaryDepartment(Employee[] employees, int department){
+        int count = 0;
+        for(Employee emp : employees)
+            if(emp.getDepartmentNumber() == department)
+                count++;
+        int avg = GetAllSalariesDepartment(employees, department) / count;
+        System.out.println(avg);
+    }
+
+    static  void IndexingSalaryDepartment(Employee[] employees, int department, int indexing) {
+        for(Employee emp : employees) {
+            if(emp.getDepartmentNumber() == department) {
+                int newSalary = emp.getSalary() + Math.round(emp.getSalary() * indexing / 100);
+                emp.setSalary(newSalary);
+            }
+        }
+    }
+
+    static void GetFIODepartment(Employee[] employees, int department){
+        for(Employee emp : employees)
+            if(emp.getDepartmentNumber() == department)
+                System.out.println(emp.getId() + " " + emp.getFirstName() + " " + emp.getLastName() + " " + emp.getPatronymic() + " " + emp.getSalary());
+    }
+
+    static void GetMinSalaryEmployee(Employee[] employees, int salary){
+        for(Employee emp : employees)
+            if(salary > emp.getSalary()) {
+                System.out.println(emp.getId() + " " + emp.getFirstName() + " " + emp.getLastName() + " " + emp.getPatronymic() + " " + emp.getSalary());
+            }
+    }
+
+    static void GetMaxSalaryEmployee(Employee[] employees, int salary){
+        for(Employee emp : employees)
+            if(salary < emp.getSalary()) {
+                System.out.println(emp.getId() + " " + emp.getFirstName() + " " + emp.getLastName() + " " + emp.getPatronymic() + " " + emp.getSalary());
+            }
     }
 }
